f92501c767c85762bf938c96413cabde4cbbe768
survivorhao <2737278737@qq.com>
2025-09-29 11:57:04 +0800

    my first commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(int agrc, char *argv[])
+{
+    if(argc==2)
+    {
+	printf("hello %s \n",argv[1]);
+    }
+
+    printf("hello git world \n");
+    
+    return 0;
+
+
+}
