b1ebe77a0f1e2633160a23e7694e2ef5e87a5cd1
survivorhao <2737278737@qq.com>
2025-10-04 16:52:13 +0800

    add specific content (only main branch have)

--- source/hello.c
+++ source/hello.c
@@ -2,7 +2,12 @@
 #include <stdint.h>
 #include "fun.h"
 
+void specific_main_branch_have(void)
+{
+    printf("this is only main branch have \n");
 
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +15,8 @@ int main(int argc, char *argv[])
     {
 	printf("hello %s \n",argv[1]);
     }
+    specific_main_branch_have();
+    
     func1();
     func2();
     user_hhhao_hao_edit();
