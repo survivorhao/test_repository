61d64def4397a9ec69ade2cfed4b6a2e869f0b18
survivorhao <2737278737@qq.com>
2025-10-04 09:12:05 +0800

    test commit 2

--- source/fun.c
+++ source/fun.c
@@ -25,3 +25,13 @@ void fun3(void)
 {
     printf("this is test3 \n");
 }
+
+void bsp_driver_init(void)
+{
+    printf("bsp dirver init... \n");
+
+
+    printf("bsp driver init success \n");
+    
+
+}
